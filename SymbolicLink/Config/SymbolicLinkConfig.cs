e07973d7cf02c018f3de0bd80f73652ad69e4af7
Besjan <xhikab@gmail.com>
2020-06-04 01:14:54 +0200

    Refactor

---
+++ SymbolicLink/Config/SymbolicLinkConfig.cs
@@ -0,0 +1,20 @@
+﻿namespace Cuku.Utilities
+{
+	using Cuku.ScriptableObject;
+	using Sirenix.OdinInspector;
+	using Sirenix.Utilities;
+	using System;
+
+    [Serializable]
+	[GlobalConfig("Assets/Cuku/Utilities/SymbolicLink/Config/Data", UseAsset = true)]
+    public class SymbolicLinkConfig : GlobalConfig<SymbolicLinkConfig>
+    {
+		[InfoBox("Real directory for which the link will be created for, located outside Unity project.", InfoMessageType.None)]
+		[PropertySpace, InlineEditor]
+        public StringSO RealDirectory;
+
+		[PropertySpace(50), InlineEditor]
+		[InfoBox("Parent directory where the link will be created in, located inside Unity project.", InfoMessageType.None)]
+        public StringSO ParentDirectory;
+    }
+}
---
+++ SymbolicLink/Editor/SymbolicLinkEditor.cs
@@ -0,0 +1,40 @@
+﻿namespace Cuku.Utilities
+{
+	using Sirenix.OdinInspector;
+	using Sirenix.OdinInspector.Editor;
+	using Sirenix.Utilities;
+	using Sirenix.Utilities.Editor;
+	using System.Collections.Generic;
+	using System.IO;
+	using UnityEditor;
+	using UnityEngine;
+
+	public class SymbolicLinkEditor : OdinEditorWindow
+    {
+        private static Vector2 windowSize = new Vector2(500, 280);
+
+        [MenuItem("Cuku/Utilities/Create Symlink")]
+        private static void OpenWindow()
+        {
+            var window = GetWindow<SymbolicLinkEditor>();
+
+            window.position = GUIHelper.GetEditorWindowRect().AlignCenter(windowSize.x, windowSize.y);
+            window.minSize = window.maxSize = windowSize;
+        }
+
+        protected override IEnumerable<object> GetTargets()
+        {
+            return new object[] { SymbolicLinkConfig.Instance, this };
+        }
+
+		[PropertySpace(50), Button(ButtonSizes.Large)]
+        public void CreateSymbolicLink()
+        {
+            var linkDirectory = Path.Combine(SymbolicLinkConfig.Instance.ParentDirectory.Value, new DirectoryInfo(SymbolicLinkConfig.Instance.RealDirectory.Value).Name);
+
+            var commandSymbolicLink = string.Format(@"New-Item -Path ""{0}"" -ItemType SymbolicLink -Value ""{1}""", linkDirectory, SymbolicLinkConfig.Instance.RealDirectory.Value);
+
+            commandSymbolicLink.ExecutePowerShellCommand(false, true);
+        }
+    }
+}
